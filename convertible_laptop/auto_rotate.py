b78d5bcb0a1b3ed949a497f921ab86ee9ece6ed2
B Tasker <github@bentasker.co.uk>
2024-12-08 21:05:02 +0000

    feat: adjust the rotation script to work with the ideapad

--- convertible_laptop/auto_rotate.py
+++ convertible_laptop/auto_rotate.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+#
+# Modified from the version at
+# https://gist.githubusercontent.com/ei-grad/4d9d23b1463a99d24a8d/raw/rotate.py
+#
+#
 
 from time import sleep
 from os import path as op
@@ -25,15 +30,19 @@ else:
 
 devices = check_output(['xinput', '--list', '--name-only']).splitlines()
 
-touchscreen_names = ['touchscreen', 'wacom']
-touchscreens = [i for i in devices if any(j in i.lower() for j in touchscreen_names)]
+touchscreen_names = [b'FTSC1000:00 2808:1015']
+touchscreens = [i.decode() for i in devices if any(j.lower() == i.lower() for j in touchscreen_names)]
 
 disable_touchpads = False
+'''
+There's no value in trying to disable the touchpad when the tablet is disconnected
+it just causes exceptions
 
-touchpad_names = ['touchpad', 'trackpoint']
+touchpad_names = [b'touchpad', b'trackpoint']
 touchpads = [i for i in devices if any(j in i.lower() for j in touchpad_names)]
+'''
 
-scale = float(read('in_accel_scale'))
+scale = float(read('in_accel_scale_available').split(" ")[0])
 
 g = 7.0  # (m^2 / s) sensibility, gravity trigger
 
@@ -48,19 +57,45 @@ STATES = [
      'check': lambda x, y: x <= -g},
 ]
 
+# mapping
+#
+# lambdas have been swapped from -> to:
+#
+# inverted -> right
+# normal -> left
+# right -> inverted
+# left -> normal
+#
+# Coords for left and right needed to be switched
+
+
+STATES = [
+    {'rot': 'normal', 'coord': '1 0 0 0 1 0 0 0 1', 'touchpad': 'enable',
+     'check': lambda x, y: x >= g},
+    {'rot': 'inverted', 'coord': '-1 0 1 0 -1 1 0 0 1', 'touchpad': 'disable',
+     'check': lambda x, y: x <= -g},
+    {'rot': 'left', 'coord': '0 -1 1 1 0 0 0 0 1', 'touchpad': 'disable',
+     'check': lambda x, y: y <= -g},
+    {'rot': 'right', 'coord': '0 1 0 -1 0 1 0 0 1', 'touchpad': 'disable',
+     'check': lambda x, y: y >= g},
+]
+
+
 
 def rotate(state):
     s = STATES[state]
     check_call(['xrandr', '-o', s['rot']])
-    for dev in touchscreens if disable_touchpads else (touchscreens + touchpads):
+    for dev in touchscreens:
+        #print(f'Configuring {dev}')
         check_call([
             'xinput', 'set-prop', dev,
             'Coordinate Transformation Matrix',
         ] + s['coord'].split())
+    '''
     if disable_touchpads:
         for dev in touchpads:
             check_call(['xinput', s['touchpad'], dev])
-
+    '''
 
 def read_accel(fp):
     fp.seek(0)
@@ -73,7 +108,6 @@ if __name__ == '__main__':
     accel_y = bdopen('in_accel_y_raw')
 
     current_state = None
-
     while True:
         x = read_accel(accel_x)
         y = read_accel(accel_y)
@@ -81,7 +115,8 @@ if __name__ == '__main__':
             if i == current_state:
                 continue
             if STATES[i]['check'](x, y):
+                #print(STATES[i]['rot'])
                 current_state = i
                 rotate(i)
                 break
-        sleep(1)
+        sleep(0.5)
